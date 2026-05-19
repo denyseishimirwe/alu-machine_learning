5015b95a0e9e0179d4b7307383a651816a64d1c1
denyseishimirwe <d.ishimirwe@alustudent.com>
2026-05-19 15:13:39 +0200

    Add linear algebra tasks: transpose, matrix ops, numpy functions

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,16 +1,5 @@
 #!/usr/bin/env python3
-"""Module that performs matrix multiplication with numpy."""
 import numpy as np
 
-
 def np_matmul(mat1, mat2):
-    """Perform matrix multiplication.
-
-    Args:
-        mat1: a numpy.ndarray.
-        mat2: a numpy.ndarray.
-
-    Returns:
-        A new numpy.ndarray that is the product mat1 x mat2.
-    """
     return np.matmul(mat1, mat2)
